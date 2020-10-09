ea148a21c3fcf53803ba9a321cca3655bf897dd3
Korede Adegboye <korede.adegboye@hotmail.com>
2020-10-09 15:37:31 -0400

    Updates

---
+++ precache-manifest.19cafb4582e337d0b6c096a3ac10f4f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d80e1279f9bce9866d227dbdb8d054",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c828452d7056e8f9622f",
+    "url": "/portfolio/static/css/main.f3b0f7df.chunk.css"
+  },
+  {
+    "revision": "8696449d99cc6a1edc7b",
+    "url": "/portfolio/static/js/2.23ef2763.chunk.js"
+  },
+  {
+    "revision": "718fa98a469a94012f350cb6e7e01f06",
+    "url": "/portfolio/static/js/2.23ef2763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c828452d7056e8f9622f",
+    "url": "/portfolio/static/js/main.fefff1c9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
